5dc4181f767e40e4c87f1131232fa22dfbd7e7d4
NipunDharmarathne <e18077@eng.pdn.ac.lk>
2024-02-05 15:49:13 +0530

    MappingRobotRandomMoving4 Implemented assuming starting points and heading directions of robots were unknown. Have to implement map merging

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,11 +12,23 @@ import Robots.samples.MappingRobot;
 import Robots.samples.MappingRobotRandomMoving1;
 import Robots.samples.MappingRobotRandomMoving2;
 import Robots.samples.MappingRobotRandomMoving3;
+import Robots.samples.MappingRobotRandomMoving4;
 import Robots.samples.MazeFollowingRobot;
 import Robots.samples.ObstacleAvoidRobot;
 import Robots.samples.SampleRobot;
 
+import java.util.Random;
+
 public class App {
+
+    private static boolean isObstacle(int x, int y, int[][] obstacles) {
+        for (int[] pair : obstacles) {
+            if (pair[0] == x && pair[1] == y) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
 
@@ -50,13 +62,49 @@ public class App {
             // Robot robot = new MazeFollowingRobot(10, 27, 27, 90);
             // new Thread(robot).start();
 
+            int[] headingDirections = {-90, 0, 90, 180}; //left, front, right, back
+            int[] x = {-81, -63, -45, -27, -9, 9, 27, 45, 63, 81};
+            int[] y = {-81, -63, -45, -27, -9, 9, 27, 45, 63, 81};
+
+            int[][] obstacles = {
+                {9, 9},
+                {45, 45},
+                {-63, 45},
+                {63, -45},
+                {-45, -45}
+            };
+
+            int randomHeading, randomX, randomY;
+            Random random = new Random();
+            randomHeading = random.nextInt(headingDirections.length);
+            do {
+                randomX = random.nextInt(x.length);
+                randomY = random.nextInt(y.length);
+            } while (isObstacle(x[randomX], y[randomY], obstacles));
+    
+            System.out.println("Robot1 x:" + x[randomX] + ", y:" + y[randomY] + ", heading:" + headingDirections[randomHeading]);
+    
+
             // Start 2 mapping robots
-            Robot robot1 = new MappingRobotRandomMoving3(1, -81, -81, 90);
+            Robot robot1 = new MappingRobotRandomMoving4(1, x[randomX], y[randomY], headingDirections[randomHeading]);
             new Thread(robot1).start();
 
-            Robot robot2 = new MappingRobotRandomMoving3(2, 81, 81, 90);
+
+
+
+            randomHeading = random.nextInt(headingDirections.length);
+            do {
+                randomX = random.nextInt(x.length);
+                randomY = random.nextInt(y.length);
+            } while (isObstacle(x[randomX], y[randomY], obstacles));
+
+            System.out.println("Robot2 x:" + x[randomX] + ", y:" + y[randomY] + ", heading:" + headingDirections[randomHeading]);
+
+            Robot robot2 = new MappingRobotRandomMoving4(2, x[randomX], y[randomY], headingDirections[randomHeading]);
             new Thread(robot2).start();   
             
+            
+
             
             // // Start a swarm of robots
             // int[] robotList = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
