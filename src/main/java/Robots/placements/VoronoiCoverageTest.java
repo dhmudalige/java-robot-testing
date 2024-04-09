0c78adda9aaf73d04f4c8db5069c0f3043cd970c
dhmudalige <e18227@eng.pdn.ac.lk>
2024-04-09 07:30:28 +0530

    updated Voronoi Test V3
    Separately defined the classes

--- src/main/java/Robots/placements/VoronoiCoverageTest.java
+++ src/main/java/Robots/placements/VoronoiCoverageTest.java
@@ -1,5 +1,6 @@
 package Robots.placements;
 
+import Robots.algorithms.VoronoiCoverage;
 import Robots.samples.SampleRobot;
 import swarm.configs.MQTTSettings;
 import swarm.robot.Robot;
@@ -9,51 +10,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 import java.util.Random;
+
+import static Robots.algorithms.VoronoiCoverage.placeRobots;
 
 public class VoronoiCoverageTest {
 
     public static final int GRID_LENGTH = 10;
     public static final double COVERAGE_RADIUS = 18.0;
-
-    public void placeRobots(Robot[] robots, int n, int p, double r) {
-
-        // Iteratively refine points
-        boolean changed;
-        do {
-            changed = false;
-            for (Robot robot : robots) {
-                double newX = robot.coordinates.getX();
-                double newY = robot.coordinates.getY();
-
-                for (Robot otherRobot : robots) {
-                    if (robot != otherRobot) {
-                        double X = robot.coordinates.getX();
-                        double otherX = otherRobot.coordinates.getX();
-
-                        double Y = robot.coordinates.getY();
-                        double otherY = otherRobot.coordinates.getY();
-
-                        double distance = Math.sqrt(Math.pow(X - otherX, 2) + Math.pow(Y - otherY, 2));
-                        if (distance < 2 * r) {
-                            // Move the robot away from the other robot
-                            double moveX = (X - otherX) * (2 * r - distance) / distance;
-                            double moveY = (Y - otherY) * (2 * r - distance) / distance;
-                            newX += moveX;
-                            newY += moveY;
-                            changed = true;
-                        }
-                    }
-                }
-                // Update robot position
-                // robot.x = newX;
-                // robot.y = newY;
-                robot.communicationInterrupt("<Voronoi-Test> Updated ROBOT" + robot.getId() + " position");
-            }
-        } while (changed);
-
-    }
+    public static final int ROBOTS_COUNT = 3;
 
     public static void main(String[] args) {
 
@@ -75,24 +42,32 @@ public class VoronoiCoverageTest {
             MQTTSettings.channel = props.getProperty("channel", "v1");
             reader.close();
 
-            // // Start a single robot
-            // Robot robot = new MazeFollowingRobot(10, 9, 9, 90);
-            // new Thread(robot).start();
-
-            // Start a swarm of robots
-            Random random = new Random();
-
-            int[] robotIDList = {0, 1, 2, 3, 4};
+            // Start a robot swarm
+//            List<VoronoiCoverage.RobotPosition> rPositions = placeRobots(GRID_LENGTH, ROBOTS_COUNT, COVERAGE_RADIUS);
 
             int startX = 0, startY = 0, startHeading = 90;
 
-            Robot[] vr = new VirtualRobot[robotIDList.length];
+            Robot[] vr = new VirtualRobot[ROBOTS_COUNT];
+//            Random delta = new Random();
+            Random random = new Random();
 
-            for (int i = 0; i < robotIDList.length; i++) {
+            /*for (int i = 0; i < ROBOTS_COUNT; i++) {
+                RobotPosition p = rPositions.get(i);
+
+                System.out.println("Robot "+ p.robotId + ": x=" + p.x + ", y=" + p.y);
+
+
+                vr[i] = new SampleRobot(p.robotId, startX + p.x, startY - p.y,
+                        startHeading + delta.nextInt(8) * 45);
+
+                new Thread(vr[i]).start();
+            }*/
+
+            for (int i = 0; i < ROBOTS_COUNT; i++) {
                 int delta = random.nextInt() % 18;
                 int sign = (random.nextInt() % 2 == 0) ? 1 : -1;
 
-                vr[i] = new SampleRobot(robotIDList[i], startX + Math.pow(sign * delta, 3) % 81, startY - sign * delta * 2,
+                vr[i] = new SampleRobot(i, startX + Math.pow(sign * delta, 3) % 81, startY - sign * delta * 2,
                         startHeading + delta);
                 new Thread(vr[i]).start();
             }
